7a4de4326ef644785a9a30ca12014658bd8548fa
JayBisht21 <jaybisht212004@gmail.com>
2026-04-02 12:53:13 +0530

    Refactor app2.py to improve functionality and UI

--- app2.py
+++ app2.py
@@ -122,7 +122,7 @@ with st.container(border=True):
     with col1:
         st.image("https://cdn-icons-png.flaticon.com/512/3135/3135673.png", width=80) 
     with col2:
-        st.title("Ai Resume Sorting Scanner and Dashboard")
+        st.title("AI RESUME SORTING SCANNER & DASHBOARD")
         st.markdown("**AI-Powered Resume Screening & Bias-Aware Analytics Dashboard**")
         # Added your name here!
         st.markdown("*Architecture & Engineering by **Jay Bisht & Sawan Saklani***")
